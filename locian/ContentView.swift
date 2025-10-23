7638524eb53945df5478e603f04e63eb9b87d87a
pinni vamshi krishna <pinnivamshikrishna@gmail.com>
2025-10-23 23:54:52 +0530

    Initial Commit

---
+++ locian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  locian
+//
+//  Created by vamshi krishna pinni on 23/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ locian/locianApp.swift
@@ -0,0 +1,17 @@
+//
+//  locianApp.swift
+//  locian
+//
+//  Created by vamshi krishna pinni on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct locianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
